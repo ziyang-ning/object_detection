f3ce22cb57cdaa57e57fc451c9f649ea9bc319a8
ZiyangNing <callmejasonbro@gmail.com>
2024-05-08 11:07:01 -0700

    mostly working

--- python_algoproto/nonlinear_detection.py
+++ python_algoproto/nonlinear_detection.py
@@ -23,12 +23,19 @@ def convert_jpg_to_matrix(filename):
         matrix = np.array(gray_img)
         return matrix
 
-# def matrix2buff(mat, image_shape):
-#     buff_vec = []
-#     for i in range(len(mat[0])):
-#         buff_vec = 
-#     return
+def image_to_1d_array(image_path):
+    # Open the image file
+    with Image.open(image_path) as img:
+        # Convert image to grayscale if it's not already
+        grayscale_img = img.convert('L')
+        
+        # Convert the image data to a numpy array
+        np_array = np.array(grayscale_img)
+        
+        # Flatten the 2D array to 1D
+        flat_np_array = np_array.flatten()
 
+    return flat_np_array
 
 # Function to read the CSV data into a 2D NumPy array for a grayscale image
 def read_csv_to_grayscale_image(filename, image_shape):
@@ -85,29 +92,29 @@ def plot_bin(image, thresh):
     plt.show()
 
 
+
 ##############
 ## TESTS
 ##############
 
 # Configuration
-csv_filename = 'data/graydata/gray2_240X240.csv'  # Replace with your CSV file's path
-# csv_filename = 'data/graydata/FOV_verification3.csv'  # Replace with your CSV file's path
-# csv_filename = 'data/graydata/all_black.csv'  # Replace with your CSV file's path
 image_shape = (240, 240)  # Image dimensions (height, width)
 
-# Load the image from the CSV
-image = read_csv_to_grayscale_image(csv_filename, image_shape)
+jpg = 1 #a bool for input data type, when 0 it means input is a csv file
+if(jpg):
+    # filename = 'multi_roundedsquare.jpg'
+    filename = 'multiobj_challenge.jpg'
+    # filename = 'two_obj_noise.jpg'
+    gray_buff = image_to_1d_array(filename)
+    image = convert_jpg_to_matrix(filename)
+else:
+    filename = 'data/graydata/all_black.csv'  # Replace with your CSV file's path
+    # filename = 'data/graydata/gray2_240X240.csv'  # Replace with your CSV file's path
+    # filename = 'data/graydata/FOV_verification3.csv'  # Replace with your CSV file's path
+    image = read_csv_to_grayscale_image(filename, image_shape)
+    gray_buff = make_gray_buffer(filename, image_shape)
 
-# Plot the image in grayscale
-plt.imshow(image, cmap='gray')
-plt.axis('off')  # Hide the axes
-plt.show()
 
-# prepare the buffer
-gray_buff = make_gray_buffer(csv_filename, image_shape)
-# print(gray_buff)
-
-# plot the buffer
 plot_buffer(gray_buff, image_shape)
 
 ################
@@ -299,7 +306,7 @@ def fam1_p1(gray_buff, image_shape, threshold):
     print("the y_vals_of_x_medians vec: ", y_vals_of_x_medians)
     return x_median_final, y_median_final
 
-def fam1_p2(gray_buff, image_shape, threshold):
+def fam2_p1(gray_buff, image_shape, threshold):
     pixcount = 0    #using this iterator, might be more efficent than doing mult everytime
     x_medians = []   # size will the the y length of the image
     y_vals_of_x_medians = []
@@ -437,7 +444,7 @@ def fam1_p2(gray_buff, image_shape, threshold):
     return x_median_final, y_median_final
 
 #I think it's better to redo the whole thing, draw it out, too many branches to think about
-def fam2_p1(gray_buff, image_shape, threshold):
+def fam1_p2(gray_buff, image_shape, threshold):
     pixcount = 0    #using this iterator, might be more efficent than doing mult everytime
     x_medians = []   # size will the the y length of the image
     y_vals_of_x_medians = []
@@ -576,15 +583,166 @@ def fam2_p1(gray_buff, image_shape, threshold):
     print("the y_vals_of_x_medians vec: ", y_vals_of_x_medians)
     return x_median_final, y_median_final
 
+def fam2_p2(gray_buff, image_shape, threshold):
+    pixcount = 0    #using this iterator, might be more efficent than doing mult everytime
+    x_medians = []   # size will the the y length of the image
+    y_vals_of_x_medians = []
+    arr_pixvisited = [0] * image_shape[0]*image_shape[1]     # purely for visualization
+    numpixvisited = 0
+
+    detected = 0 #make this a bool in C++
+
+    num_conseq_black_rows = 0
+    last_row_black = 0      #Let this be a bool in C++ implementation
+
+    # for j in range(image_shape[0]):
+    j = 0
+    while( j < image_shape[0]):
+        all_x_gt_thresh = []    #max size will be the x length of the image
+        all_x_gt_thresh_real_length = 0
+
+        blob_start_index = -1  #the left most starting index of the first blob detected
+
+        if(blob_start_index != -1):
+            i = blob_start_index
+        else:
+            i = 0
+
+        while (i < image_shape[1] and pixcount < (image_shape[0] *image_shape[1])):
+
+            if(gray_buff[pixcount] <= threshold or gray_buff[pixcount+1] <= threshold or gray_buff[pixcount+2] <= threshold):
+                if(i+10 < image_shape[1]):
+
+                    #for visualization, yeah shitty code i know
+                    if(gray_buff[pixcount] <= threshold):
+                        numpixvisited = numpixvisited + 1
+                        arr_pixvisited[pixcount] = 255
+                    elif(gray_buff[pixcount+1] <= threshold):
+                        numpixvisited = numpixvisited + 2
+                        arr_pixvisited[pixcount] = 255
+                        arr_pixvisited[pixcount+1] = 255
+                    elif(gray_buff[pixcount+2] <= threshold):
+                        numpixvisited = numpixvisited + 3
+                        arr_pixvisited[pixcount] = 255
+                        arr_pixvisited[pixcount+1] = 255
+                        arr_pixvisited[pixcount+2] = 255
+
+                    i = i + 10
+                    pixcount = pixcount + 10
+                    
+
+                else:
+                    #for visualization, yeah shitty code i know
+                    if(gray_buff[pixcount] <= threshold):
+                        numpixvisited = numpixvisited + 1
+                        arr_pixvisited[pixcount] = 255
+                    elif(gray_buff[pixcount+1] <= threshold):
+                        numpixvisited = numpixvisited + 2
+                        arr_pixvisited[pixcount] = 255
+                        arr_pixvisited[pixcount+1] = 255
+                    elif(gray_buff[pixcount+2] <= threshold):
+                        numpixvisited = numpixvisited + 3
+                        arr_pixvisited[pixcount] = 255
+                        arr_pixvisited[pixcount+1] = 255
+                        arr_pixvisited[pixcount+2] = 255
+
+                    pixcount = pixcount + image_shape[1] - i
+                    i = image_shape[1]
+                    numpixvisited = numpixvisited + 1
+                    
+            ## if 3 whites
+            ##------MIGHT GO OUT OF BOUND HERE----- ACTUALLY NO, but it woun't behave as expected)
+            # elif gray_buff[pixcount]>threshold and gray_buff[pixcount+1]>threshold and gray_buff[pixcount+2]>threshold:
+            else:
+
+                detected = 1
+
+                if(i - 20 < 0):
+                    blob_start_index = 0
+                else:
+                    blob_start_index = i - 20
+
+                x = i
+                all_x_gt_thresh.append(x)
+                all_x_gt_thresh.append(x+1)
+                all_x_gt_thresh.append(x+2)
+                all_x_gt_thresh_real_length = all_x_gt_thresh_real_length + 3
+                i = i + 3
+                pixcount = pixcount + 3
+                numpixvisited = numpixvisited + 3
+
+                #for visualization
+                arr_pixvisited[pixcount-1] = arr_pixvisited[pixcount-2] =  arr_pixvisited[pixcount-3] = 255
+
+                #can do this later, track the largest blob
+                # if(index_largestBlob == -1):
+                #     index_largestBlob = i
+                #     index_currentBlob = i
+
+                #searach to the right
+                while(gray_buff[pixcount] > threshold and i < image_shape[1]):
+                    x = i
+                    all_x_gt_thresh.append(x)
+                    all_x_gt_thresh_real_length = all_x_gt_thresh_real_length + 1
+                    i = i + 1
+                    pixcount = pixcount + 1
+                    numpixvisited = numpixvisited + 1
+
+                    #for visualization
+                    arr_pixvisited[pixcount-1] = 255
+
+                #this means that the current pixcount is already checked and it's not white
+                if i != image_shape[1]:
+                    i = i + 1
+                    pixcount = pixcount + 1
+                    numpixvisited = numpixvisited + 1
+
+                    #for visualization
+                    arr_pixvisited[pixcount-1] = 255
+
+                ##CONTENT ADDED HERE
+                #moving to the next row!
+                pixcount = pixcount + image_shape[1] - i
+                i = i + len(image[1]) #automatically moves to the next row
+
+        # The median function
+        # I don't think 1 pixel difference matters, so not going to do even odd, improves speed
+        if(all_x_gt_thresh_real_length != 0):
+            x_medians.append(all_x_gt_thresh[all_x_gt_thresh_real_length // 2 ]) # floor division
+            y_vals_of_x_medians.append(j)
+
+            last_row_black = 0
+        else:
+            num_conseq_black_rows = num_conseq_black_rows + 1
+            last_row_black = 1
+
+        if(all_x_gt_thresh_real_length == 0 and blob_start_index == -1 and detected):
+            break
+
+        if(num_conseq_black_rows > 2 and last_row_black and j+3 < image_shape[0]):
+            j = j + 2
+            pixcount = pixcount + 2 * image_shape[1]
+        
+        j = j + 1
+
+    ###----Added condition for all black or almost all black)
+    ### already implemented in arduino code
+    if(len(x_medians) >= 5):
+        x_median_final = x_medians[len(x_medians) // 2]
+        y_median_final = y_vals_of_x_medians[len(x_medians) // 2]
+    else:
+        x_median_final = 0
+        y_median_final = 0
+
+    #visualization
+    plot_buffer(arr_pixvisited, image_shape)
+    print("num pix visited: ", numpixvisited)
+    print("the y_vals_of_x_medians vec: ", y_vals_of_x_medians)
+    return x_median_final, y_median_final
 
 
 # Example usage:
 threshold = 210  # Define your threshold
-
-# median_x, median_y = process_image(gray_buff, image_shape, threshold)
-# print(f"Median X Coordinate: {median_x}")
-# print(f"Median Y Coordinate: {median_y}")
-# plot_image_with_median_circle(gray_buff, image_shape, median_x, median_y)
 
 plot_bin(image, threshold)
 
@@ -597,14 +755,19 @@ print("median_x: ", median_x, "median_y: ", median_y)
 #tested and works, ready for ESP implementation
 nonlin_x, nonlin_y = fam1_p1(gray_buff, image_shape, threshold)
 plot_image_with_median_circle(gray_buff, image_shape, nonlin_x, nonlin_y)
-print("nonlin_x: ", nonlin_x, "nonlin_y: ", nonlin_y)
+print("fam1_p1: ", nonlin_x, "fam1_p1: ", nonlin_y)
+
+#family 2 propsed 1
+nonlin_x, nonlin_y = fam2_p1(gray_buff, image_shape, threshold)
+plot_image_with_median_circle(gray_buff, image_shape, nonlin_x, nonlin_y)
+print("fam2_p1 x: ", nonlin_x, "fam2_p1 y: ", nonlin_y)
 
 #family 1 propsed 2
 nonlin_x, nonlin_y = fam1_p2(gray_buff, image_shape, threshold)
 plot_image_with_median_circle(gray_buff, image_shape, nonlin_x, nonlin_y)
 print("fam1_p2 x: ", nonlin_x, "fam1_p2 y: ", nonlin_y)
 
-#family 2 propsed 1
-nonlin_x, nonlin_y = fam2_p1(gray_buff, image_shape, threshold)
+#family 2 propsed 2
+nonlin_x, nonlin_y = fam2_p2(gray_buff, image_shape, threshold)
 plot_image_with_median_circle(gray_buff, image_shape, nonlin_x, nonlin_y)
-print("fam2_p1 x: ", nonlin_x, "fam2_p1 y: ", nonlin_y)
+print("fam2_p2 x: ", nonlin_x, "fam2_p2 y: ", nonlin_y)
